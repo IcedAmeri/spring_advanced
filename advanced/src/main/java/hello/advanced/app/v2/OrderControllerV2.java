8828caa3c934f15a1fda7aea8471cedd28b94d50
IcedAmeri <seokhyunkho@gmail.com>
2021-12-03 14:29:28 +0900

    [예제 만들기] 로그 추적기 V2 - 적용

--- advanced/src/main/java/hello/advanced/app/v2/OrderControllerV2.java
+++ advanced/src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -1,7 +1,7 @@
 package hello.advanced.app.v2;
 
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderControllerV2 {
 
     private final OrderServiceV2 orderService;
-    private final HelloTraceV1 trace;
+    private final HelloTraceV2 trace;
 
-    @GetMapping("/v1/request")
+    @GetMapping("/v2/request")
     public String request(String itemId) {
 
         TraceStatus status = null;
         try {
             status = trace.begin("OrderController.request()");
-            orderService.orderItem(itemId);
+            orderService.orderItem(status.getTraceId(), itemId);
             trace.end(status);
             return "ok";
         } catch (Exception e) {
--- advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -1,7 +1,8 @@
 package hello.advanced.app.v2;
 
+import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,14 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
-    private final HelloTraceV1 trace;
+    private final HelloTraceV2 trace;
 
-    public void orderItem(String itemId) {
+    public void orderItem(TraceId traceId, String itemId) {
 
         TraceStatus status = null;
         try {
-            status = trace.begin("OrderService.orderItem()");
-            orderRepository.save(itemId);
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
             trace.end(status);
         } catch (Exception e) {
             trace.exception(status, e);
